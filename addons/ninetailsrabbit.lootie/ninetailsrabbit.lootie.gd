b945d5dd904ae27db86709fc5c468f0a48112b96
ninetailsrabbit <danieldigitalspace@proton.me>
2024-10-11 10:07:40 +0100

    updated the plugin.gd so new resources are added

--- addons/ninetailsrabbit.lootie/ninetailsrabbit.lootie.gd
+++ addons/ninetailsrabbit.lootie/ninetailsrabbit.lootie.gd
@@ -12,10 +12,13 @@ func _enter_tree() -> void:
 	if not DirAccess.dir_exists_absolute(MyPluginSettings.PluginTemporaryReleaseUpdateDirectoryPath):
 		DirAccess.make_dir_recursive_absolute(MyPluginSettings.PluginTemporaryReleaseUpdateDirectoryPath)
 	
-	add_custom_type("LootieTable", "Node", preload("src/loot_table.gd"), preload("assets/lootie.svg"))
 	add_custom_type("LootItem", "Resource", preload("src/loot_item.gd"), null)
-	add_custom_type("LootItemRarity", "Resource", preload("src/loot_item_rarity.gd"), null)
+	add_custom_type("LootItemRarity", "Resource", preload("src/probability_modes/loot_item_rarity.gd"), null)
+	add_custom_type("LootItemWeight", "Resource", preload("src/probability_modes/loot_item_weight.gd"), null)
+	add_custom_type("LootItemChance", "Resource", preload("src/probability_modes/loot_item_chance.gd"), null)
 	add_custom_type("LootTableData", "Resource", preload("src/loot_table_data.gd"), null)
+	
+	add_custom_type("LootieTable", "Node", preload("src/loot_table.gd"), preload("assets/lootie.svg"))
 	add_autoload_singleton("LootieGlobal", "src/lootie_global.gd")
 	
 
@@ -29,6 +32,8 @@ func _exit_tree() -> void:
 	remove_autoload_singleton("LootieGlobal")
 	remove_custom_type("LootTableData")
 	remove_custom_type("LootItemRarity")
+	remove_custom_type("LootItemWeight")
+	remove_custom_type("LootItemChance")
 	remove_custom_type("LootItem")
 	remove_custom_type("LootieTable")
 
