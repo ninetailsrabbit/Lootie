bd92d926034ce82c716769f61e532f5f8502f597
ninetailsrabbit <danieldigitalspace@proton.me>
2025-03-04 11:07:31 +0000

    updated plugin to godot 4.4

--- addons/ninetailsrabbit.lootie/ninetailsrabbit.lootie.gd
+++ addons/ninetailsrabbit.lootie/ninetailsrabbit.lootie.gd
@@ -1,17 +1,8 @@
 @tool
 extends EditorPlugin
 
-const UpdateNotifyToolScene = preload("updater/update_notify_tool.tscn")
-
-var update_notify_tool_instance: Node
 
 func _enter_tree() -> void:
-	MyPluginSettings.set_update_notification()
-	_setup_updater()
-	
-	if not DirAccess.dir_exists_absolute(MyPluginSettings.PluginTemporaryReleaseUpdateDirectoryPath):
-		DirAccess.make_dir_recursive_absolute(MyPluginSettings.PluginTemporaryReleaseUpdateDirectoryPath)
-	
 	add_custom_type("LootItem", "Resource", preload("src/loot_item.gd"), null)
 	add_custom_type("LootItemRarity", "Resource", preload("src/probability_modes/loot_item_rarity.gd"), null)
 	add_custom_type("LootItemWeight", "Resource", preload("src/probability_modes/loot_item_weight.gd"), null)
@@ -23,12 +14,6 @@ func _enter_tree() -> void:
 	
 
 func _exit_tree() -> void:
-	MyPluginSettings.remove_settings()
-	
-	if update_notify_tool_instance:
-		update_notify_tool_instance.free()
-		update_notify_tool_instance = null
-		
 	remove_autoload_singleton("LootieGlobal")
 	
 	remove_custom_type("LootTableData")
@@ -39,9 +24,3 @@ func _exit_tree() -> void:
 	remove_custom_type("LootItem")
 	
 	remove_custom_type("LootieTable")
-
-## Update tool referenced from https://github.com/MikeSchulze/gdUnit4/blob/master/addons/gdUnit4
-func _setup_updater() -> void:
-	if MyPluginSettings.is_update_notification_enabled():
-		update_notify_tool_instance = UpdateNotifyToolScene.instantiate()
-		Engine.get_main_loop().root.add_child.call_deferred(update_notify_tool_instance)
